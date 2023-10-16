a8a266fb96d59e9eb3d272f2ff366617971bd810
maryam <Reemaali9048@gmail.com>
2023-10-16 16:33:55 +0300

    complete assigment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+      const rightNumbers = dodger.style.right.replace("px", "");
+      const right = parseInt(leftNumbers, 10);
+  
+      dodger.style.right = `${left - 1}px`;
+    }
+  });
